fc35bd798b2c638190d48dfb5c6aebcb1e929a2c
Alexander Belov <git@belov.by>
2014-12-19 00:32:28 +0300

    Add adaptive contrast based on area standart deviation

--- contrast.py
+++ contrast.py
@@ -1,15 +1,36 @@
 import cv2
 import numpy as np
 
-class Area:
-    n1, n2 = 2, 2
+class Config:
+    n1, n2 = 3, 3
     m1, m2 = 2, 2
+    n_min, n_max = 0.13, 0.77
+    # n_min: 0.1 ... 0.3
+    # n_max: 0.7 ... 0.9
+    fixed_kns = 0.8
+    kns_type = 'std' # 'fixed', 'std' or 'entropy'
+
+    min_std = 0
+    max_std = float('inf')
+
+def get_min_max_std(img):
+    width, height = img.shape
+    for i in range(0, width):
+        for j in range(0, height):
+            i_min, i_max, j_min, j_max = get_indices(i, j, width, height)
+            area = img[i_min:i_max, j_min:j_max]
+            std = np.std(area)
+
+            if std < Config.min_std:
+                Config.min_std = std
+            if std > Config.max_std:
+                Config.max_std = std
 
 def get_indices(i, j, width, height):
-    i_min = i - Area.n1
-    i_max = i + Area.n2
-    j_min = j - Area.m1
-    j_max = j + Area.m2
+    i_min = i - Config.n1
+    i_max = i + Config.n2
+    j_min = j - Config.m1
+    j_max = j + Config.m2
 
     if i_min < 0:
         i_min = 0
@@ -25,8 +46,17 @@ def get_indices(i, j, width, height):
 def contrast(z, zc):
     return abs(z-zc)/float(z+zc)
 
-def contrast_gain(cz):
-    return cz**0.8
+def get_kns(area):
+    if Config.kns_type == 'fixed':
+        return Config.fixed_kns
+    if Config.kns_type == 'std':
+        return (np.std(area) - Config.min_std) / float(Config.max_std - Config.min_std)
+
+def get_adaptive_power(area):
+    return Config.n_min+(Config.n_max-Config.n_min)*get_kns(area)
+
+def contrast_gain_adaptive(cz, area):
+    return cz**get_adaptive_power(area)
 
 def get_new_pixel(z, zc, c):
     if z < zc:
@@ -34,10 +64,13 @@ def get_new_pixel(z, zc, c):
     else:
         return int(zc*(1+c)/float(1-c))
 
-
 img = cv2.imread('lenna.bmp', 0)
 width, height = img.shape
 img_new = cv2.imread('lenna.bmp', 0)
+
+if Config.kns_type == 'std':
+    get_min_max_std(img)
+    print Config.min_std, Config.max_std
 
 for i in range(0, width):
     for j in range(0, height):
@@ -47,10 +80,10 @@ for i in range(0, width):
         pixel_brightness = img[i,j]
         pixel_contrast = contrast(pixel_brightness, average_brightness)
 
-        gained_contrast = contrast_gain(pixel_contrast)
+        gained_contrast = contrast_gain_adaptive(pixel_contrast, area)
         new_pixel = get_new_pixel(pixel_brightness, average_brightness, gained_contrast)
         img_new[i,j] = new_pixel
-
+        print new_pixel
 
 cv2.imwrite('lenna_adaptive.bmp', img_new)
 cv2.imshow('image',img_new)
